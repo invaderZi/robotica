5ee97253d9be4895e76664e129772a7047518878
zi <40426593+invaderZi@users.noreply.github.com>
2025-08-28 15:53:01 -0300

    passo 7

--- pratica_1/src/cpp_pubsub/src/publisher_member_function.cpp
+++ pratica_1/src/cpp_pubsub/src/publisher_member_function.cpp
@@ -8,5 +8,12 @@ using namespace std::chrono_literals;
 
 class MinimalPublisher : public rclcpp::Node
 {
-    
+    public:
+        MinimalPublisher()
+        : Node("minimal_publisher"), count_(0)
+        {
+        publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+        timer_ = this->create_wall_timer(
+        500ms, std::bind(&MinimalPublisher::timer_callback, this));
+        }
 };
